885ea2d363b68c6b60ea3f5df9845b7ccf88eabe
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-04-17 12:41:30 +0200

    Add botid example

--- examples/botid.lua
+++ examples/botid.lua
@@ -3,10 +3,13 @@ local botid = 0
 
 local function saveid(extra, result)
   botid = result.id
+  print("bot id", botid)
 end
 
-function tdbot_update_callback()
+function tdbot_update_callback(res)
   if res._ == "updateNewMessage" then
     tdbot_function({_ = "getMe"}, saveid)
   end
 end
+
+require 'tdbot'
