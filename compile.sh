4a41cb3476cfe84ca4fa696269118fe6ff52517e
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-09-18 15:28:00 +0200

    travis fix

--- compile.sh
+++ compile.sh
@@ -8,6 +8,8 @@ make -j6 VERBOSE=1
 sudo make install
 cd ../../
 mkdir build
+git submodule init
+git submodule update
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 ..
 cmake --build .
