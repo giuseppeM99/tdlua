369f48e5935cbf5fe0998a12f52274c8990f6dfa
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-09-20 14:23:46 +0200

    no lua 5.1

--- compile.sh
+++ compile.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-
-git clone https://github.com/xiph/opus
-cd opus
-git checkout v1.2.1
-./autogen.sh
-./configure
-make
-sudo make install
-cd ..
-
 git clone https://github.com/tdlib/td
 cd td
 mkdir build
@@ -25,5 +15,5 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 ..
 cmake --build .
 
-curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so" -F caption=$LUA_VERSION
-lua ../examples/uploadtravis.lua
+curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so" -F caption="$LUA_VERSION"
+$LUA ../examples/uploadtravis.lua
