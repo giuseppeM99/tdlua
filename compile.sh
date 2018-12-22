1f3d8d1f5aae041fdfc27ea9eb74aa77c364dc83
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-12-22 12:46:07 +0100

    Fix build

--- compile.sh
+++ compile.sh
@@ -23,7 +23,7 @@ if [ -n "$TDLUA_CALLS" ]; then
     git submodule update
 fi
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 -DTDULA_CALLS=$TDLUA_CALLS..
+cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 -DTDULA_CALLS=$TDLUA_CALLS ..
 cmake --build .
 
 curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so" -F caption="$LUA_VERSION CALLS $TDLUA_CALLS"
